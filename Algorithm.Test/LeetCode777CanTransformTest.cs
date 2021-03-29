66c63e18e74efdc4713226f49d75cfc0c32ae9d4
HeroWong <hero_wong@outlook.com>
2021-03-29 19:03:52 +0800

    swap-adjacent-in-lr-string

---
+++ Algorithm.Test/LeetCode777CanTransformTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Algorithm.Test
+{
+    [TestClass]
+    public class LeetCode777CanTransformTest
+    {
+        [TestMethod]
+        [DataRow("RXXLRXRXL", "XRLXXRRLX", true)]
+        [DataRow("LXXLXRLXXL", "XLLXRXLXLX", false)]
+        [DataRow("RXR", "XXR", false)]
+        [DataRow("XXXXXLXXXX", "LXXXXXXXXX", true)]
+        [DataRow("XXXLXXXXXX", "XXXLXXXXXX", true)]
+        public void MaxSumTwoNoOverlap(string start, string end, bool expected)
+        {
+            var leetCode = new LeetCode777CanTransform();
+            bool actual = leetCode.CanTransform(start, end);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithm/LeetCode777CanTransform.cs
@@ -0,0 +1,50 @@
+﻿namespace Algorithm
+{
+    //https://leetcode-cn.com/problems/swap-adjacent-in-lr-string/
+    public class LeetCode777CanTransform
+    {
+        public bool CanTransform(string start, string end)
+        {
+            if (start.Replace("X", string.Empty) == end.Replace("X", string.Empty))
+            {
+                int j = 0;
+                for (int i = 0; i < start.Length; i++)
+                {
+                    if (start[i] == 'L')
+                    {
+                        while (end[j] != 'L')
+                        {
+                            j++;
+                        }
+                        if (j > i)
+                        {
+                            return false;
+                        }
+                        j++;
+                    }
+                }
+                j = 0;
+                for (int i = 0; i < start.Length; i++)
+                {
+                    if (start[i] == 'R')
+                    {
+                        while (end[j] != 'R')
+                        {
+                            j++;
+                        }
+                        if (j < i)
+                        {
+                            return false;
+                        }
+                        j++;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
