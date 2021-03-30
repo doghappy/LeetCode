fc4dd82a2be8029dabb27dbf25e295b8b94977b6
HeroWong <hero_wong@outlook.com>
2021-03-30 15:42:59 +0800

    LeetCode2AddTwoNumbers

---
+++ Algorithm.Test/LeetCode2AddTwoNumbersTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Test
+{
+    [TestClass]
+    public class LeetCode2AddTwoNumbersTest
+    {
+        [TestMethod]
+        public void TwoSumTest1()
+        {
+            var l1 = new LeetCode2AddTwoNumbers.ListNode(2);
+            l1.next = new LeetCode2AddTwoNumbers.ListNode(4);
+            l1.next.next = new LeetCode2AddTwoNumbers.ListNode(3);
+
+            var l2 = new LeetCode2AddTwoNumbers.ListNode(5);
+            l2.next = new LeetCode2AddTwoNumbers.ListNode(6);
+            l2.next.next = new LeetCode2AddTwoNumbers.ListNode(4);
+
+            var result = LeetCode2AddTwoNumbers.AddTwoNumbers(l1, l2);
+            Assert.AreEqual(7, result.val);
+            Assert.AreEqual(0, result.next.val);
+            Assert.AreEqual(8, result.next.next.val);
+            Assert.IsNull(result.next.next.next);
+        }
+
+        [TestMethod]
+        public void TwoSumTest2()
+        {
+            var l1 = new LeetCode2AddTwoNumbers.ListNode(9);
+            l1.next = new LeetCode2AddTwoNumbers.ListNode(9);
+            l1.next.next = new LeetCode2AddTwoNumbers.ListNode(9);
+            l1.next.next.next = new LeetCode2AddTwoNumbers.ListNode(9);
+            l1.next.next.next.next = new LeetCode2AddTwoNumbers.ListNode(9);
+            l1.next.next.next.next.next = new LeetCode2AddTwoNumbers.ListNode(9);
+            l1.next.next.next.next.next.next = new LeetCode2AddTwoNumbers.ListNode(9);
+
+            var l2 = new LeetCode2AddTwoNumbers.ListNode(9);
+            l2.next = new LeetCode2AddTwoNumbers.ListNode(9);
+            l2.next.next = new LeetCode2AddTwoNumbers.ListNode(9);
+            l2.next.next.next = new LeetCode2AddTwoNumbers.ListNode(9);
+
+            var result = LeetCode2AddTwoNumbers.AddTwoNumbers(l1, l2);
+            Assert.AreEqual(8, result.val);
+            Assert.AreEqual(9, result.next.val);
+            Assert.AreEqual(9, result.next.next.val);
+            Assert.AreEqual(9, result.next.next.next.val);
+            Assert.AreEqual(0, result.next.next.next.next.val);
+            Assert.AreEqual(0, result.next.next.next.next.next.val);
+            Assert.AreEqual(0, result.next.next.next.next.next.next.val);
+            Assert.AreEqual(1, result.next.next.next.next.next.next.next.val);
+            Assert.IsNull(result.next.next.next.next.next.next.next.next);
+        }
+    }
+}
---
+++ Algorithm/LeetCode2AddTwoNumbers.cs
@@ -0,0 +1,60 @@
+﻿namespace Algorithm
+{
+    //https://leetcode-cn.com/problems/add-two-numbers/
+    public class LeetCode2AddTwoNumbers
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var root = new ListNode();
+            var c1 = l1;
+            var c2 = l2;
+            var node = root;
+            while (true)
+            {
+                int val = node.val;
+                if (c1 != null)
+                    val += c1.val;
+                if (c2 != null)
+                    val += c2.val;
+                if (val > 9)
+                {
+                    node.val = val - 10;
+                    node.next = new ListNode(1);
+                }
+                else
+                {
+                    node.val = val;
+                }
+                if (c1 != null)
+                    c1 = c1.next;
+                if (c2 != null)
+                    c2 = c2.next;
+                if (c1 == null && c2 == null)
+                {
+                    break;
+                }
+                else
+                {
+                    if (node.next == null)
+                    {
+                        node.next = new ListNode();
+                    }
+                    node = node.next;
+                }
+            }
+            return root;
+        }
+    }
+}
