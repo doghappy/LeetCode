611565bda5044cc939ce5fb5654deaef0d12a855
HeroWong <hero_wong@outlook.com>
2021-03-31 19:33:04 +0800

    FindMedianSortedArrays

---
+++ Algorithm.Test/LeetCode4FindMedianSortedArraysTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Test
+{
+    [TestClass]
+    public class LeetCode4FindMedianSortedArraysTest
+    {
+        [TestMethod]
+        [DataRow(new[] { 1, 3 }, new[] { 2 }, 2d)]
+        [DataRow(new[] { 1, 2 }, new[] { 3, 4 }, 2.5d)]
+        public void LengthOfLongestSubstringTest(int[] nums1, int[] nums2, double expected)
+        {
+            double actual = LeetCode4FindMedianSortedArrays.FindMedianSortedArrays(nums1, nums2);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Algorithm/LeetCode4FindMedianSortedArrays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Algorithm
+{
+    //https://leetcode-cn.com/problems/median-of-two-sorted-arrays/
+    public class LeetCode4FindMedianSortedArrays
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int totalLength = nums1.Length + nums2.Length;
+            int a = (totalLength - 1) / 2;
+            int b = totalLength / 2;
+            int v1 = Find(nums1, nums2, 0, 0, a);
+            int v2 = Find(nums1, nums2, 0, 0, b);
+            return (v1 + v2) * 1d / 2;
+        }
+
+        private static int Find(int[] nums1, int[] nums2, int i, int j, int c)
+        {
+            if (i >= nums1.Length)
+            {
+                return nums2[j + c];
+            }
+            if (j >= nums2.Length)
+            {
+                return nums1[i + c];
+            }
+            if (c == 0)
+            {
+               return Math.Min(nums1[i], nums2[j]);
+            }
+            else if (nums1[i] > nums2[j])
+            {
+                return Find(nums1, nums2, i, ++j, --c);
+            }
+            else
+            {
+                return Find(nums1, nums2, ++i, j, --c);
+            }
+        }
+    }
+}
